7391edaf4e06415ae977e833e99ae85a75e071d4
江林锴 <jianglinkai@vzan.com>
2024-04-28 19:26:13 +0800

    docs: ignore

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const gitPush = async (commit, addList) => {
         return '规范错误'
     }
     await processExec('git add ' + addList.join(' '));
-    const gitAdd = $`git diff --staged`.stdout;
-    console.log('gitadd-->', gitAdd)
+    const gitAdd = await $`git diff --staged`;
+    console.log('gitadd-->', gitAdd.stdout)
     await processExec(`git commit -m "${commit}"`);
     await processExec('git pull');
     await processExec('git push');
