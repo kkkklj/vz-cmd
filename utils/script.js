0403feafe47bd0bf8a9c096a2b75c5ab001ea537
jianglinkai <jianglinkai@vzan.com>
2025-11-09 11:28:36 +0800

    feat: 优化处理

--- utils/script.js
+++ utils/script.js
@@ -3,7 +3,7 @@ import { readFileSync, writeFileSync } from 'fs';
 import generator from '@babel/generator'
 // import traverse from "@babel/traverse";
 import { parse, traverse, types } from '@babel/core'
-const methodVueDataAssignmentExpressionPathList = []
+const vueDataAssignmentExpressionPathList = []
 const replaceThisPathList = []
 function computedCallName(name) {
   return `__computed__${name}`
@@ -14,8 +14,9 @@ function getThisPathNextMemberKey(path) {
 /**
  * 检查当前的this是否可替换为this.data
  */
-function checkThisCanReplace(path) {
+function checkThisCanReplace(path, whiteList) {
   if (!path.parent.property) return false
+  if (whiteList && whiteList.includes(path.parent.property.name)) return false
   if (/^__computed__/.test(path.parent.property.name)) return false
   if (['setData', '$route', '$toast'].includes(path.parent.property.name)) {
     return false
@@ -43,6 +44,20 @@ function assignmentExpressionToSetData(path) {
 function replaceThisToThisData(path) {
   path.replaceWith(types.memberExpression(types.thisExpression(), types.identifier('data')))
 }
+function getObjKeys(node) {
+  return node.value.properties.map(node => node.key.name)
+}
+// 检查当前的path是否vue里的为this.xxx = xxx里的this
+function checkIsVueDataAssignmentForThisPath(path) {
+  const thisAssignmentExpressionPath = path.findParent(_path => _path.node.type === 'AssignmentExpression'
+    && _path.node.left?.property?.name === path.parent.property.name
+    && _path.node.left?.object?.type === 'ThisExpression'
+  )
+  if (thisAssignmentExpressionPath && thisAssignmentExpressionPath.type === 'AssignmentExpression') {
+    return thisAssignmentExpressionPath
+  }
+  return null
+}
 function compileScript() {
   const blockShouldUpdateComputedMap = new Map()
   const dataMap = new Map()
@@ -51,6 +66,7 @@ function compileScript() {
     sourceType: 'module'
   });
   const scriptBody = ast.program.body
+  
   const exportDefault = scriptBody.find(i => i.type === 'ExportDefaultDeclaration')
   if (exportDefault) {
     const exportProps = exportDefault.declaration.properties
@@ -61,8 +77,10 @@ function compileScript() {
     const vueData = exportProps.find(i => i.key.name === 'data')
     const vueComponents = exportProps.find(i => i.key.name === 'components')
     const vueMethods = exportProps.find(i => i.key.name === 'methods')
+    const vueCreatedHook = exportProps.find(i => i.key.name === 'created')
     const vueDataReturn = vueData.body.body.find(i => i.type === 'ReturnStatement')
-    const vueDataReturnProps = vueDataReturn.argument.properties
+    const vueDataReturnProps = vueDataReturn?.argument?.properties
+    const vueDataWatcher = new VueDataWatcher(vueDataReturnProps)
     vueDataReturnProps.forEach(returnPropItem => {
       dataMap.set(returnPropItem.key.name, [])
     })
@@ -95,50 +113,17 @@ function compileScript() {
       },
       ThisExpression(path) {
         if (path.findParent(path => path.node === vueComputed)) {
-          if (path.findParent(path => methodComputed.includes(path.node)) && checkThisCanReplace(path)) {
+          if (path.findParent(path => methodComputed.includes(path.node)) && checkThisCanReplace(path, getObjKeys(vueMethods))) {
             !replaceThisPathList.includes(path) && replaceThisPathList.push(path)
           }
-          const computedUseVueDateKey = getThisPathNextMemberKey(path)
-          const dataSubArray = dataMap.get(computedUseVueDateKey)
-          if (dataSubArray) {
-            const computedCallExpressionStr = `this.${computedCallName(computedUseVueDateKey)}()`
-            if (dataSubArray.find(i => i.__callExpStr__ === computedCallExpressionStr)) {
-              return
-            }
-            const computedCallAst = parse(computedCallExpressionStr, {
-              sourceType: 'script'
-            }).program.body[0]
-            computedCallAst.__callExpStr__ = computedCallExpressionStr
-            dataSubArray.push(computedCallAst)
-          }   
-        } else if (path.findParent(path => path.node === vueMethods)) {
+          vueDataWatcher.listenVueDataChangeInComputed(path)
+        } else if (path.findParent(path => [vueMethods, vueCreatedHook].includes(path.node))) {
           // this赋值处理，当前的path为this
-          const methodVueDataAssignmentExpressionPath = path.findParent(_path => _path.node.type === 'AssignmentExpression' && _path.node.left?.property?.name === path.parent.property.name)
-          if (methodVueDataAssignmentExpressionPath && methodVueDataAssignmentExpressionPath.type === 'AssignmentExpression') { 
-            const methodAssignmentThisKey = getThisPathNextMemberKey(path)
-            const dataSubArray = dataMap.get(methodAssignmentThisKey)
-            if (dataSubArray?.length) {
-              const findBlock = path.findParent(path => path.node.type === 'BlockStatement')
-              /** @type {*[]} */
-              let blockShouldUpdateComputed = blockShouldUpdateComputedMap.get(findBlock)
-              if (!blockShouldUpdateComputed) {
-                blockShouldUpdateComputed = []
-                blockShouldUpdateComputedMap.set(findBlock, blockShouldUpdateComputed)
-              }
-              
-              dataSubArray.forEach(item => {
-                if (blockShouldUpdateComputed.includes(item)) return
-                blockShouldUpdateComputed.push(item)
-                // item.__deployExpStr__ = methodAssignmentThisKey
-              })
-
-              // const __ = methodVueDataAssignmentExpressionPath
-              // const _thisPath = path
-            
-              // debugger
-            }
-            if (!methodVueDataAssignmentExpressionPathList.includes(methodVueDataAssignmentExpressionPath)) {
-              methodVueDataAssignmentExpressionPathList.push(methodVueDataAssignmentExpressionPath)
+          const thisAssignmentExpressionPath = checkIsVueDataAssignmentForThisPath(path)
+          if (thisAssignmentExpressionPath) { 
+            vueDataWatcher.vueDataUpdate(path)
+            if (!vueDataAssignmentExpressionPathList.includes(thisAssignmentExpressionPath)) {
+              vueDataAssignmentExpressionPathList.push(thisAssignmentExpressionPath)
             }
           }
         }
@@ -165,14 +150,11 @@ function compileScript() {
         }
       }
     })
-    blockShouldUpdateComputedMap.forEach((shouldUpdateComputed, blockStatement) => {
-      const insertBeforeNodeIndex = blockStatement.node.body.findLastIndex(node => node.type !== 'ReturnStatement')
-      blockStatement.node.body.splice(insertBeforeNodeIndex + 1, 0, ...shouldUpdateComputed)
-    })
-    methodVueDataAssignmentExpressionPathList.forEach((path) => assignmentExpressionToSetData(path))
+    vueDataWatcher.mountedShouldUpdateCallAst()
+    vueDataAssignmentExpressionPathList.forEach((path) => assignmentExpressionToSetData(path))
     traverse(ast, {
       ThisExpression(path) {
-        if (path.findParent(path => path.node === vueMethods) && checkThisCanReplace(path)) {
+        if (path.findParent(path => [vueCreatedHook, vueMethods].includes(path.node)) && checkThisCanReplace(path, getObjKeys(vueMethods))) {
           !replaceThisPathList.includes(path) && replaceThisPathList.push(path)
         }
       }
@@ -187,7 +169,6 @@ function compileScript() {
         sourceType: 'script'
       }).program.body[0]
       attachedScope.body.body.push(computedCallAst)
-      
     })
     // const vueData = [...methodProps].find(i => i.key.name === 'data')
   }
@@ -198,4 +179,72 @@ function compileScript() {
   writeFileSync('./tttttttt.js', output)
   debugger
 }
+class VueDataWatcher {
+  constructor(vueDataReturnProps) {
+    this.vueDataUpdateBlock = new Map()
+    this.vueDataSubscribeMap = new Map()
+    this.blockShouldCallMap = new Map()
+    vueDataReturnProps.forEach(returnPropItem => {
+      this.vueDataSubscribeMap.set(returnPropItem.key.name, [])
+    })
+  }
+  /**
+   * 
+   * @param {NodePath<types.Node>} path 
+   * @param {'computed'} type 
+   */
+  listenVueDataChangeInComputed(thisPath) {
+    let useVueDataKey = getThisPathNextMemberKey(thisPath)
+    const isBindName =  computedCallName(useVueDataKey)
+    const vueDataScribeList = this.vueDataSubscribeMap.get(useVueDataKey)
+    if (vueDataScribeList) {
+      if (vueDataScribeList.find(i => i.__uniKey__ === isBindName)) return
+      const callExpressionStr = `this.${isBindName}()`
+      const callAst = parse(callExpressionStr, {
+        sourceType: 'script'
+      }).program.body[0]
+      callAst.__uniKey__ = isBindName
+      vueDataScribeList.push(callAst)
+    }
+  }
+  /**
+   * 修改vue data时
+   */
+  vueDataUpdate(thisPath) {
+    const vueDataAssignmentExpressionPath = thisPath.findParent(_path => _path.node.type === 'AssignmentExpression'
+      && _path.node.left?.property?.name === thisPath.parent.property.name
+      && _path.node.left?.object?.type === 'ThisExpression'
+    )
+    // 赋值表达式
+    if (vueDataAssignmentExpressionPath && vueDataAssignmentExpressionPath.type === 'AssignmentExpression') {
+      // 赋值的data key
+      const assignmentKey = getThisPathNextMemberKey(thisPath)
+      const vueDataScribeList = this.vueDataSubscribeMap.get(assignmentKey)
+      // 有订阅了vue data的才需要更新
+      if (vueDataScribeList?.length) {
+        const curBlock = thisPath.findParent(path => path.node.type === 'BlockStatement')
+        /** @type {*[]} */
+        let shouldCallAstList = this.blockShouldCallMap.get(curBlock)
+        if (!shouldCallAstList) {
+          shouldCallAstList = []
+          this.blockShouldCallMap.set(curBlock, shouldCallAstList)
+        }
+        vueDataScribeList.forEach(callAst => {
+          if (shouldCallAstList.includes(callAst)) return
+          shouldCallAstList.push(callAst)
+        })
+      }
+    }
+  }
+  /**
+   * 挂载刷新的computed等方法
+   */
+  mountedShouldUpdateCallAst() {
+    this.blockShouldCallMap.forEach((shouldBeCallAstList, blockStatement) => {
+      const insertBeforeNodeIndex = blockStatement.node.body.findLastIndex(node => node.type !== 'ReturnStatement')
+      blockStatement.node.body.splice(insertBeforeNodeIndex + 1, 0, ...shouldBeCallAstList)
+    })
+  }
+}
 compileScript()
+
